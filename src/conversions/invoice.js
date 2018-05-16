27536ad8c4aea3fb3516060d98d8c17edd729d82
Pieterjan Fiers <pieterjan.fiers@citymesh.com>
2018-05-16 20:28:06 +0200

    Implement basic conversion

--- src/conversions/invoice.js
+++ src/conversions/invoice.js
@@ -1,1 +1,23 @@
 /** will convert a complete invoice */
+let convert = function (input, options) {
+    let output = {}
+
+    output['boekjaar'] = options.boekjaar
+    output['periode'] = options.periode
+    output['boekstuknummer'] = input.number
+    output['Omschrijving: Kopregel'] = null
+    output['boekdatum'] = input.date
+    output['vervaldatum'] = null
+    output['valuta'] = 'EUR'
+    output['wisselkoers'] = null
+    if (input.invoice_term_id == 4425) {
+        output['betalingsvoorwaarde'] = 14
+    } else if (input.invoice_term_id == 4826) {
+        output['betalingsvoorwaarde'] = 30
+    }
+
+
+    return output;
+}
+
+export default convert
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import {config} from '../config/index'
 import stringify from 'csv-stringify'
 import mock from './mock'
 import fs from 'fs'
-stringify(mock, function (err, output) {
+import convertor from './conversions/invoice'
+
+let converted = []
+
+/** convert each result and add to array */
+mock.forEach(function (input) {
+    converted.push(convertor(input, {boekjaar: 2018, periode: 8}))
+})
+
+/** convert JSON to csv and write to file */
+stringify(converted, function (err, output) {
     fs.writeFile('./export/test.csv', output, function (err) {
         console.log(err)
     })
